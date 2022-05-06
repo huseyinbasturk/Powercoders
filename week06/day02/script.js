f4d302c19105d2f8111a52c78d29ab1741da39ef
huseyinbasturk <hb.basturk41@gmail.com>
2022-05-06 11:51:42 +0200

    started to do list

---
+++ week06/day02/script.js
@@ -0,0 +1,79 @@
+/*must have requirements 
+    title 
+    list of to do 
+    possibility to add new new to-do : input - button 
+    change order / set priority 
+    change status od to do (open / done - in progress)
+
+
+inputs - todo (list of object: string, number, boolean)
+    1 todo - object
+    many todos - array of objects
+outputs - array of todos rensered as list items
+
+steps - 
+    add to do
+        enter input
+        submit input
+        validate input (is it empty, does exact string exist as open to do(repetition))
+         > invalid: return error message & enter input again / valid: move on to next step
+        create object with input string, number(default add to the bottom of list), boolean(default isDone : false)
+        add to the list (push it to the array)
+
+    display to do list
+    mark as done
+    order / set priority
+
+    for the buttons - event listen
+
+    1. variables
+        not in yet - user input how to order
+    */
+
+const todo = document.querySelector('input');
+const addButton = document.querySelector('button');
+const list = document.querySelector('ol');
+
+let todos = [];
+
+//check for duplicates
+let isInputUnique = (newItem) => {
+    //loop through the todos and compare each item to newItem
+    //if one item is equal to new item > return false
+    //else return true
+
+}
+
+let addToDo = () => {
+    //get value from input field
+    let newItem = todo.value;
+    //validate value from input field,check for duplication
+    if(isInputUnique(newItem)) {
+        //create instance
+        //push to todos list
+        todos.push(new Todo(newItem,todos.length));
+    } else {
+        //returns error message
+    }
+
+    
+}
+
+let displayList = todos => {
+    //loop through to do list
+    //for each object in the list, we render HTML li-tag
+    //each li tag will be appended to do ol-DOM element 'list'
+
+}
+addButton.addEventListener('click', addToDo);
+
+function Todo(text, id, isDone=false){
+    this.text = text;
+    this.id = id;
+    this.isDone = isDone;
+    this.setId  = function() {
+
+    }
+}
+
+
