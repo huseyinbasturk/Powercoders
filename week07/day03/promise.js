010b60d09a3ec60fc50a7982611a7771dad76e6e
huseyinbasturk <hb.basturk41@gmail.com>
2022-05-13 16:55:02 +0200

    last

--- week07/day03/promise.js
+++ week07/day03/promise.js
@@ -1,1 +1,15 @@
-let promise = new Promise(function)
+let names = [{
+    name: "Susanne",
+    surname: 'Koenig',
+    age: 88
+},{
+    name: "Mesut",
+    surname: 'Asci',
+    age: 88
+}];
+let allSurnames = names.map(user => user.name == 'Susanne' ? {...user, age:22 } : user);
+console.log(allSurnames);
+
+let numbers = [1,2,3,4,5,6,7];
+let power2 = numbers.map(number => number * number);
+console.log(power2);
